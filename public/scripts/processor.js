4e9ea2905251f12fd8fff943888c44c045c52e6e
CNwwsun <wwsun@thoughtworks.com>
2016-06-12 10:49:32 +0800

    for cycle page; Wayde

--- public/scripts/processor.js
+++ public/scripts/processor.js
@@ -14,17 +14,59 @@
         }
     };
 
-    function findSummary(HTMLEle) {
-        var summaryTable = $(HTMLEle).find('.summary');
-        var completedStories = $(summaryTable).find('.number-big').text();
-        var stages = $(summaryTable).find('.stage-bar');
+    function getStagesDetail(ele){
+        var stages = $(ele).find('.stage-bar');
         var stagesDetail = [];
         $.each(stages, function(index, value){
             var stageName = $(value).find('title').text();
-            var stageValue = $(value).find('text:last').text();
+            var stageValue = $(value).find('text:last').text().replace(' days', '');
             stagesDetail.push({name: stageName, value: stageValue});
         });
-        console.log(stagesDetail);
+        return stagesDetail;
+    };
+
+    function findSummary(HTMLEle) {
+        var summaryTable = $(HTMLEle).find('.summary');
+        var completedStories = $(summaryTable).find('.number-big').text();
+
+        return {total: completedStories, stage: getStagesDetail($(summaryTable))};
+    };
+
+    function getCardDetailInfoByLink(link){
+        $.ajax({
+            url: link,
+            success: function (result) {
+                if (result.isOk == false) {
+                    console.log(result.message);
+                };
+            },
+            async: false
+        });
+    };
+
+    function findStories(HTMLEle){
+        var storiesTable = $(HTMLEle).find('.stories');
+        var stories = $(storiesTable).find('.cycle');
+
+        var cycles = [];
+        $.each(stories, function(index, value){
+            var link = $(value).find('.card-number a').attr('href');
+            //var detail = getCardDetailInfoByLink(link);
+            var number = $(value).find('.card-number a').text();
+            var name = $(value).find('.card-name').text();
+            var duration = $(value).find('.duration').text().replace(' days', '');
+            var stage = getStagesDetail($(value).find('.breakdown'));
+
+            cycles.push({
+                link: link,
+                number: number,
+                name: name,
+                duration: duration,
+                stage: stage
+            });
+        });
+
+        return cycles;
     };
 
     ct.analyze = function (HTML) {
@@ -32,7 +74,13 @@
 
         var HTMLElement = $.parseHTML(HTML);
         var summary = findSummary(HTMLElement);
-        console.log(steps);
+        var stories = findStories(HTMLElement);
+
+        return {
+            steps: steps,
+            summary: summary,
+            stories: stories
+        };
 
     };
 })(window.ct = window.ct || {})
